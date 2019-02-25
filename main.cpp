e1e4c886b403ae4040e59661921764027d690819
RobinTheSprite <madscientistak@gmail.com>
2019-02-25 14:06:18 -0900

    Creating processes

--- main.cpp
+++ main.cpp
@@ -15,12 +15,15 @@ int main(int argc, TCHAR *argv[])
     STARTUPINFO startupInfo;
     PROCESS_INFORMATION processInfo;
 
+    ZeroMemory(&startupInfo, sizeof(startupInfo));
+    startupInfo.cb = sizeof(startupInfo);
+
 	char directory[MAX_PATH];
     while(true)
     {
-		size_t bufferLength = GetCurrentDirectory(MAX_PATH, directory);
+		GetCurrentDirectory(MAX_PATH, directory);
 		string dir(directory);
-		cout << dir << "=>";
+		cout << dir << " >>=>";
 
         string inputLine;
         getline(cin, inputLine);
@@ -29,19 +32,24 @@ int main(int argc, TCHAR *argv[])
         string partOfCommand;
         while(inputStream >> partOfCommand)
         {
-            char * prt = const_cast<char *>("notepad");
-            CreateProcess(nullptr, prt, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInfo);
+            if (partOfCommand == "exit")
+            {
+                break;
+            }
+            char * prt = const_cast<char *>(partOfCommand.c_str());
+            if (!CreateProcess(nullptr, prt, NULL, NULL, false, NORMAL_PRIORITY_CLASS, NULL, NULL, &startupInfo, &processInfo))
+            {
+                cout << "No command called " << prt << endl;
+            }
 			//ShellExecute(NULL, NULL, partOfCommand.c_str(), "", NULL, SW_NORMAL);
         }
-
-		if (partOfCommand == "exit")
-		{
-			break;
-		}
+        if (partOfCommand == "exit")
+        {
+            break;
+        }
     }
 
     WaitForSingleObject(processInfo.hProcess, INFINITE);
-
     CloseHandle( processInfo.hProcess );
     CloseHandle( processInfo.hThread );
 

b63bb434bfcdab47aa416787ee532e242fcf2eb9
Mark Underwood <mtunderwood@alaska.edu>
2019-03-19 18:09:25 -0800

    Finish permit command, but still cannot deny permissions

--- file-ops/permit.cpp
+++ file-ops/permit.cpp
@@ -2,37 +2,90 @@
 // Created by Mark on 3/7/2019.
 //
 #include "aclapi.h"
+#include "lmcons.h"
+#include "sddl.h"
 #include <iostream>
+
+void printError()
+{
+    LPTSTR errorMessage;
+    DWORD errorCode = GetLastError();
+    FormatMessage( FORMAT_MESSAGE_FROM_SYSTEM,
+                   nullptr,
+                   errorCode,
+                   0,
+                   (LPTSTR)&errorMessage,
+                   0,
+                   nullptr );
+    std::cout << "Error Setting Permissions: " << errorMessage << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
     if (argc > 1)
     {
+        PACL dacl, newDacl = nullptr;
+        PSECURITY_DESCRIPTOR security;
+        GetNamedSecurityInfo(
+                argv[1],
+                SE_FILE_OBJECT,
+                DACL_SECURITY_INFORMATION,
+                nullptr,
+                nullptr,
+                &dacl,
+                nullptr,
+                &security
+                );
+
+        PSID sid;
+        DWORD lenSid = SECURITY_MAX_SID_SIZE;
+        sid = LocalAlloc(LMEM_FIXED, lenSid);
+        if (CreateWellKnownSid(WinWorldSid, nullptr, sid, &lenSid) == 0)
+        {
+            printError();
+        }
+
+        EXPLICIT_ACCESS rule;
+        ZeroMemory(&rule, sizeof(EXPLICIT_ACCESS));
+        rule.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
+        BuildTrusteeWithSid(&rule.Trustee, sid);
+
         if (strcmp(argv[2], "read") == 0)
         {
-           if(SetNamedSecurityInfo(argv[1],
-                   SE_FILE_OBJECT,
-                   DACL_SECURITY_INFORMATION,
-                   nullptr,
-                   nullptr,
-                   nullptr,
-                   nullptr)
-                != ERROR_SUCCESS)
-           {
-               LPTSTR errorMessage;
-               DWORD errorCode = GetLastError();
-               FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER |
-                              FORMAT_MESSAGE_FROM_SYSTEM |
-                              FORMAT_MESSAGE_IGNORE_INSERTS,
-                              nullptr,
-                              errorCode,
-                              0,
-                              (LPTSTR)&errorMessage,
-                              0,
-                              nullptr );
-               std::cout << "Error Setting Permissions: " << errorMessage << std::endl;
-           }
+            rule.grfAccessPermissions = GENERIC_READ;
+            rule.grfAccessMode = GRANT_ACCESS;
         }
+        else if (strcmp(argv[2], "write") == 0)
+        {
+            rule.grfAccessPermissions = GENERIC_WRITE;
+            rule.grfAccessMode = GRANT_ACCESS;
+        }
+        else if (strcmp(argv[2], "execute") == 0)
+        {
+            rule.grfAccessPermissions = GENERIC_EXECUTE;
+            rule.grfAccessMode = GRANT_ACCESS;
+        }
+
+        if (SetEntriesInAcl(1, &rule, dacl, &newDacl) != ERROR_SUCCESS)
+        {
+            printError();
+        }
+
+        if(SetNamedSecurityInfo(argv[1],
+                                SE_FILE_OBJECT,
+                                DACL_SECURITY_INFORMATION,
+                                nullptr,
+                                nullptr,
+                                newDacl,
+                                nullptr)
+           != ERROR_SUCCESS)
+        {
+            printError();
+        }
+
+        LocalFree(sid);
+        LocalFree(security);
+        LocalFree(newDacl);
     }
     return 0;
 }

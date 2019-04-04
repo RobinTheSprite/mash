b4554795139b74176ac3dba65695143bedacd51f
Mark Underwood <mtunderwood@alaska.edu>
2019-04-03 18:41:08 -0800

    Fix ping. Now pings.

--- ping.cpp
+++ ping.cpp
@@ -1,12 +1,15 @@
 //
 // Created by Mark on 3/15/2019.
 //
-#include <windows.h>
+#include <Windows.h>
+#include <winsock.h>
 #include <iostream>
 #include <iphlpapi.h>
-#include <icmpapi.h>
+#include <IcmpAPI.h>
+#include <inaddr.h>
 
-#pragma comment(lib, "IPHLPAPI.lib")
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
 
 //printError
 //Tell me what the last error in this thread was
@@ -27,23 +30,43 @@ void printError()
 int main(int argc, char * argv[])
 {
    HANDLE icmpHandle = IcmpCreateFile();
-//   DWORD replySize = sizeof(ICMP_ECHO_REPLY);
-//   LPVOID reply[1024];
-//    if (argc > 1)
-//    {
-//        if (IcmpSendEcho(
-//                icmpHandle,
-//                inet_addr(argv[1]),
-//                nullptr,
-//                0,
-//                PIP_OPTION_INFORMATION(),
-//                reply,
-//                replySize,
-//                1000)
-//               == 0)
-//        {
-//            printError();
-//        }
-//    }
+   DWORD replySize = sizeof(ICMP_ECHO_REPLY);
+   LPVOID reply[1024];
+    if (argc > 1)
+    {
+        unsigned long ipAddress = 0;
+
+        ipAddress = inet_addr(argv[1]);
+        if(ipAddress == 0)
+        {
+            std::cout << "That was not an IP address" << std::endl;
+        }
+
+        for (auto i = 0; i < 5; ++i)
+        {
+            int retVal = IcmpSendEcho(
+                    icmpHandle,
+                    ipAddress,
+                    nullptr,
+                    0,
+                    nullptr,
+                    reply,
+                    replySize,
+                    1000);
+            if (retVal == 0)
+            {
+                printError();
+            }
+            else
+            {
+                auto theReply = (PICMP_ECHO_REPLY)reply;
+                in_addr replyAddress{};
+                replyAddress.S_un.S_addr = theReply->Address;
+
+                std::cout << "Ping Received from " << inet_ntoa(replyAddress)
+                          << " in " << theReply->RoundTripTime << " milliseconds" << std::endl;
+            }
+        }
+    }
     IcmpCloseHandle(icmpHandle);
 }

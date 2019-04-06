5a537a0e4ed11014b15940b49411f3f4d8db6642
Mark Underwood <mtunderwood@alaska.edu>
2019-04-06 14:16:30 -0800

    Move error printing code to its own file.

---
+++ mash-error.cpp
@@ -0,0 +1,24 @@
+// mash-error.cpp
+// Created by Mark on 4/6/2019.
+//
+
+#include <Windows.h>
+#include <iostream>
+
+//printError
+//Tell me what the last error in this thread was
+void printError()
+{
+    LPTSTR errorMessage = nullptr;
+    DWORD errorCode = GetLastError();
+    FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER
+                   | FORMAT_MESSAGE_FROM_SYSTEM
+                   | FORMAT_MESSAGE_IGNORE_INSERTS,
+                   nullptr,
+                   errorCode,
+                   0,
+                   (LPTSTR)&errorMessage,
+                   0,
+                   nullptr );
+    std::cout << "Error Setting Permissions: " << errorMessage << std::endl;
+}
---
+++ mash-error.h
@@ -0,0 +1,10 @@
+// mash-error.h
+// Created by Mark on 4/6/2019.
+//
+
+#ifndef MASH_MASH_ERROR_H
+#define MASH_MASH_ERROR_H
+
+void printError();
+
+#endif //MASH_MASH_ERROR_H
